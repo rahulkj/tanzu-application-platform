ebc59912d49dae4ca76fdd9f89010d69bd1997db
Rahul Jain <rahulkj@gmail.com>
2022-11-02 18:40:26 -0500

    Adding a script to help upgrade TAP

--- scripts/tap-upgrade.sh
+++ scripts/tap-upgrade.sh
@@ -134,36 +134,8 @@ download_tanzu_application_platform() {
    tanzu_network_logout
 }
 
-install_tanzu_plugins() {
-   pushd ${TANZU_CLI_DIR}
-      export TANZU_CLI_NO_INIT=true
-      tanzu plugin install all -l .
-   popd
-}
-
 docker_login_to_tanzunet() {
    docker login registry.tanzu.vmware.com -u ${TAP_TANZU_REGISTRY_USERNAME} -p ${TAP_TANZU_REGISTRY_PASSWORD}
-}
-
-configure_psp_for_tkgs(){
-   kubectl create clusterrolebinding default-tkg-admin-privileged-binding --clusterrole=psp:vmware-system-privileged --group=system:authenticated
-}
-
-install_tkg_essentials() {
-   kubectl create namespace kapp-controller
-
-   kubectl create secret generic kapp-controller-config \
-      --namespace kapp-controller \
-      --from-file caCerts=${HARBOR_CA_CERT_PATH}
-
-   pushd ${TANZU_ESSENTIALS_DIR}
-      export INSTALL_BUNDLE=${TANZU_ESSENTIALS_BUNDLE}
-      export INSTALL_REGISTRY_HOSTNAME=${TAP_TANZU_REGISTRY_HOST}
-      export INSTALL_REGISTRY_USERNAME=${TAP_TANZU_REGISTRY_USERNAME}
-      export INSTALL_REGISTRY_PASSWORD=${TAP_TANZU_REGISTRY_PASSWORD}
-
-      ./install.sh --yes
-   popd
 }
 
 copy_images_to_registry() {
@@ -177,20 +149,18 @@ copy_images_to_registry() {
       --registry-ca-cert-path ${HARBOR_CA_CERT_PATH}
 }
 
-add_tap_repository() {
+patch_lifecycle_image() {
+   kp lifecycle patch -i ${TAP_PACKAGES_IMAGE_SHA}
+
+}
+
+update_package_repository() {
    export INSTALL_REGISTRY_HOSTNAME=${TAP_HARBOR_REGISTRY_HOST}
    export INSTALL_REGISTRY_USERNAME=${TAP_HARBOR_REGISTRY_USERNAME}
    export INSTALL_REGISTRY_PASSWORD=${TAP_HARBOR_REGISTRY_PASSWORD}
    export TAP_VERSION=${TAP_VERSION}
 
-   kubectl create ns tap-install
-
-   tanzu secret registry add tap-registry \
-   --username ${INSTALL_REGISTRY_USERNAME} --password ${INSTALL_REGISTRY_PASSWORD} \
-   --server ${INSTALL_REGISTRY_HOSTNAME} \
-   --export-to-all-namespaces --yes --namespace tap-install
-
-   tanzu package repository add tanzu-tap-repository \
+   tanzu package repository update tanzu-tap-repository \
    --url ${INSTALL_REGISTRY_HOSTNAME}/${TAP_HARBOR_PROJECT}/${TAP_HARBOR_TAP_PACKAGES_REPOSITORY}:${TAP_VERSION} \
    --namespace tap-install
 
@@ -212,97 +182,18 @@ generate_tap_values() {
       --data-value-file harbor.certificate=${HARBOR_CA_CERT_PATH} > ${BASE_DIR}/config/${ENV}-tap-values-final.yaml
 }
 
-install_tap() {
-   tanzu package install tap -p tap.tanzu.vmware.com \
+upgrade_tap() {
+   tanzu package installed update tap -p tap.tanzu.vmware.com \
       -v ${TAP_VERSION} --values-file ${BASE_DIR}/config/${ENV}-tap-values-final.yaml \
       -n tap-install
-}
-
-setup_dev_namespace() {
-   export INSTALL_REGISTRY_HOSTNAME=${TAP_HARBOR_REGISTRY_HOST}
-   export INSTALL_REGISTRY_USERNAME=${TAP_HARBOR_REGISTRY_USERNAME}
-   export INSTALL_REGISTRY_PASSWORD=${TAP_HARBOR_REGISTRY_PASSWORD}
-
-   kubectl create ns ${TAP_DEV_NAMESPACE}
-
-   tanzu secret registry add tap-registry \
-   --username ${INSTALL_REGISTRY_USERNAME} --password ${INSTALL_REGISTRY_PASSWORD} \
-   --server ${INSTALL_REGISTRY_HOSTNAME} \
-   --export-to-all-namespaces --yes --namespace ${TAP_DEV_NAMESPACE}
-
-   tanzu secret registry add registry-credentials \
-   --server ${INSTALL_REGISTRY_HOSTNAME} \
-   --username ${INSTALL_REGISTRY_USERNAME} \
-   --password ${INSTALL_REGISTRY_PASSWORD} \
-   --namespace ${TAP_DEV_NAMESPACE}
-
-cat <<EOF | kubectl -n ${TAP_DEV_NAMESPACE} apply -f -
-apiVersion: v1
-kind: Secret
-metadata:
-  name: tap-registry
-  annotations:
-    secretgen.carvel.dev/image-pull-secret: ""
-type: kubernetes.io/dockerconfigjson
-data:
-  .dockerconfigjson: e30K
----
-apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: default
-secrets:
-  - name: registry-credentials
-imagePullSecrets:
-  - name: registry-credentials
-  - name: tap-registry
----
-apiVersion: rbac.authorization.k8s.io/v1
-kind: RoleBinding
-metadata:
-  name: default-permit-deliverable
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: deliverable
-subjects:
-  - kind: ServiceAccount
-    name: default
----
-apiVersion: rbac.authorization.k8s.io/v1
-kind: RoleBinding
-metadata:
-  name: default-permit-workload
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: workload
-subjects:
-  - kind: ServiceAccount
-    name: default
-EOF
-
-}
-
-function setup_git_secrets() {
-   if [[ (! -z ${GIT_USERNAME}) && (! -z ${GIT_PASSWORD}) && (! -z ${GIT_URL} ) ]]; then
-      export GIT_PASSWORD=${GIT_PASSWORD}
-      kp secret create ${TAP_GITOPS_SSH_SECRET_NAME} --git-url ${GIT_URL} \
-      --git-user ${GIT_USERNAME} --service-account ${K8S_SERVICE_ACCOUNT} \
-      --namespace ${TAP_DEV_NAMESPACE}
-   fi
 }
 
 check_for_required_clis
 validate_all_arguments
 prompt_user_kubernetes_login
-install_tanzu_plugins
 docker_login_to_tanzunet
-configure_psp_for_tkgs
-install_tkg_essentials
+patch_lifecycle_image
 copy_images_to_registry
-add_tap_repository
+update_package_repository
 generate_tap_values
-install_tap
-setup_dev_namespace
-setup_git_secrets
+upgrade_tap
